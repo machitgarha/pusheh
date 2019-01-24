39a6661d45d29990a8f761fd47a58295d202f166
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2019-01-25 01:58:13 +0330

    Add Pusheh::makeDirRecursive() method

--- src/Component/Pusheh.php
+++ src/Component/Pusheh.php
@@ -4,5 +4,14 @@ namespace MAChitgarha\Component;
 
 class Pusheh
 {
-    
+    public static function makeDirRecursive(string $dirPath, int $mode = 0777)
+    {
+        if (is_dir($dirPath))
+            return false;
+        
+        if (@mkdir($dirPath, $mode))
+            return true;
+
+        throw new \Exception("Cannot create $dirPath directory recursively");
+    }
 }
