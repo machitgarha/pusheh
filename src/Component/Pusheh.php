000538588ad7229efa47ceb48838aacdb428fe02
Mohammad Amin Chitgarha <machitgarha@outlook.com>
2019-01-25 06:36:56 +0330

    Add Pusheh::removeDirRecursive()
    For when the directory have some contents and is not empty.

--- src/Component/Pusheh.php
+++ src/Component/Pusheh.php
@@ -2,6 +2,9 @@
 
 namespace MAChitgarha\Component;
 
+use Webmozart\PathUtil\Path;
+
+
 class Pusheh
 {
     public static function createDir(string $dirPath, int $mode = 0777, bool $recursive = true)
@@ -20,11 +23,29 @@ class Pusheh
         self::createDir($dirPath, $mode, true);
     }
 
-    public static function removeDir(string $dirPath)
+    public static function removeDir(string $dirPath, bool $recursive = false)
     {
+        if ($recursive)
+            self::removeDirRecursive();
+
         if (@rmdir($dirPath))
             return true;
 
         throw new \Exception("Cannot remove $dirPath directory");
+    }
+
+    public static function removeDirRecursive(string $dirPath)
+    {
+        $dirIt = new \DirectoryIterator($dirPath);
+        $content;
+        foreach ($dirIt as $content) {
+            if ($content->isDot())
+                continue;
+            if ($content->isDir())
+                self::removeDirRecursive($content->getPathname());
+            if ($content->isFile())
+                unlink($content->getPathname());
+        }
+        self::removeDir($dirPath);
     }
 }
